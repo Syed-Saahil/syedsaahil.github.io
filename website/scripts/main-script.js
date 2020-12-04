d78e1a55360cf1ee523345a4e861c41eaee17f92
Syed Saahil <S.Saahil8438@student.leeds.beckett.ac.uk>
2020-12-04 22:18:57 +0000

    first commit

---
+++ website/scripts/main-script.js
@@ -0,0 +1,12 @@
+function anscssCheck() {
+    document.getElementById("answers-css").innerHTML = "The correct answers are: center, flex-end, flex-start, space-between, space-around";
+}
+
+function ansjsCheck() {
+    document.getElementById("answers-js").innerHTML = "The correct answer is B!";
+}
+
+function ansquizCheck() {
+    document.getElementById("answers-quiz").innerHTML = "Q1: A, Q2: B, Q3: A, Q4: A, Q5: A, Q6: C, Q7: B, Q8: B, Q9: B";
+
+}
